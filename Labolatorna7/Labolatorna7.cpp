d79a10711e15571569c8e0344605d910a6edd0d4
ulian <ulian@JULY>
2024-11-17 20:19:36 +0200

    Добавьте файлы проекта.

---
+++ Labolatorna7/Labolatorna7.cpp
@@ -0,0 +1,29 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <cmath>
+
+double function(double x) {
+    if (x <= 0) {
+        return sin(cos(x));
+    }
+    else if (x > 0) {
+        return cos(log(x));
+    }
+    else {
+       
+        return NAN;
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double x;
+    std::cout << "Введіть значення x: ";
+    std::cin >> x;
+
+    double result = function(x);
+    std::cout << "Значення функції при x = " << x << " дорівнює: " << result << std::endl;
+
+    return 0;
+}
